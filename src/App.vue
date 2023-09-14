29cd139e20fb18f95070bce32f33af99fba69571
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-09-14 14:00:28 -0300

    Creating Login View

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginView/>
 </template>
 
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+
+#app{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginView from './views/LoginView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginView
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="login-card">
+        <!-- Login Inputs -->
+        <div class="user-information">
+            <form action="">
+                <input type="text" class="" name="user_name" v-model="this.user.userName">
+                <input type="password" class="" name="user_password" v-model="this.user.userPassword">
+                <button type="submit">LOGIN</button>
+            </form>  
+        </div>
+        
+        <!-- Login Image -->
+        <div class="image">
+            <img src="">  
+        </div>
+    </div>
+</template>
+
+<style>
+.login-card{
+    width: 90%;
+    height: 90%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 1fr;
+    justify-content: center;
+    align-content: center;
+    background-color: red;
+}
+
+.user-information{
+
+}
+</style>
+
+<script>
+export default {
+    name: "LoginView",
+    data(){
+        return {
+            user: {
+                userName: null,
+                userPassword: null
+            }
+        }
+    }
+}
+</script>
