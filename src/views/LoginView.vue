3aa9bd4bfc5d20f6f54c91f8d14a3799105eecdf
carloscabral656 <edu.carlos.cec@gmail.com>
2023-09-27 15:22:35 -0300

    Adding text in aside information.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="login-card"><!-- Login Image -->
         <aside class="aside-information">
-            <img src="">  
+            <div class="aside-information-container">
+                <h3 class="title-aside-information">Lorem</h3>
+                <p class="paragraph-aside-information">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe placeat maxime debitis ipsa quaerat enim quod perspiciatis itaque obcaecati tempore harum voluptatem nulla similique odio quae, quibusdam doloremque unde error!</p>  
+            </div>
         </aside>
         <!-- Login Inputs -->
         <main class="user-information">
-            <h1 class="title-information">Some Text</h1>
-
             <div class="user-information-input-container">
+                <h2 class="title-information">Hello, <span class="part-of-the-day">Good {{ this.partOfTheDay }}</span>!</h2>
                 <h1 class="title-login">Login</h1>
 
                 <label>Username</label>
@@ -16,9 +18,9 @@
                 <label>Password</label>
                 <input type="password" class="input-login" name="user-password" v-model="user.password"/>
                 
-                <a href="#" class="link-forgot-password">Forgot password?</a>
+                <a href="#" class="link link-forgot-password">Forgot password?</a>
                 <button @click="login" class="btn-login">Login</button>
-                <a href="#" class="link-create-account">Create Account</a>
+                <a href="#" class="link link-create-account">Create Account</a>
             </div>
         </main>
     </div>
@@ -47,21 +49,52 @@ body{
     grid-column: 1/3;
     border-radius: 20px 0 0 20px ;
 }
+    .aside-information-container{
+        box-sizing: border-box;
+        padding: 100px;
+        color: #fff;
+        background-image: no-repeat center/cover url('../assets/imgs/mountains_purple.webp');
+        z-index: 1;
+    }
+
+        .title-aside-information{
+            text-transform: capitalize;
+            font-size: 3rem;
+        }
+
+        .paragraph-aside-information{
+            font-size: 1.8rem;
+            text-align: justify;
+        }
+
+
+
+
 
 .user-information{
     background-color: #fff;
     grid-column: 3/4;
-    padding: 50px;
+    padding: 25px;
     border-radius: 0 20px 20px 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
     .title-information{
         width: 100%;
     }
 
+    .part-of-the-day{
+        color: #784fff;
+    }
+
     .user-information-input-container{
+        width: 75%;
         display: flex;
         flex-direction: column;
+        flex-wrap: nowrap;
     }
 
         .title-login{
@@ -70,21 +103,17 @@ body{
         }
 
         .input-login{
-            margin-bottom: 20px;
+            margin-bottom: 50px;
             border: 0;
             padding: 1rem;
             border-bottom: 1px solid black;
             outline: none;
         }
 
-        .link-forgot-password{
-            text-align: right;
-            margin-bottom: 20px;
-        }
-
         .btn-login{
             padding: 1rem;
             background-color: #7F62E0;
+            cursor: pointer;
             color: #fff;
             font-size: 1.2rem;
             border-radius: 10px;
@@ -92,7 +121,22 @@ body{
             border: 0;
         }
 
+        .link{
+            text-decoration: none;
+            font-weight: 500;
+            color: #7F62E0;
+        }
+
+        .link:hover{
+            text-decoration: underline;
+        }
+
         .link-create-account{
+            text-align: right;
+            margin-bottom: 20px;
+        }
+
+        .link-forgot-password{
             text-align: right;
             margin-bottom: 20px;
         }
@@ -112,6 +156,20 @@ export default {
             user: {
                 name: null,
                 password: null
+            }
+        }
+    },
+    computed: {
+        partOfTheDay(){
+            let hour = new Date().getHours();
+           if(hour >= 2 && hour <= 12){
+                return 'Morning'
+            } else if (hour >= 13 && hour <= 18){
+                return 'Afternoon'
+            } else if (hour >= 19 && hour <= 22){
+                return 'Evening'
+            } else {
+                return 'Night'
             }
         }
     },
