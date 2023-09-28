4914329c3af423b560734e72431ea54cb87025c9
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-09-28 13:12:03 -0300

    Altering part of the day logic

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -171,11 +171,11 @@ export default {
     computed: {
         partOfTheDay(){
             let hour = new Date().getHours();
-           if(hour >= 2 && hour <= 12){
-                return 'Morning'
-            } else if (hour >= 13 && hour <= 18){
+            if(hour >= 2 && hour <= 12){
+                    return 'Morning'
+            } else if (hour <= 18){
                 return 'Afternoon'
-            } else if (hour >= 19 && hour <= 22){
+            } else if (hour <= 22){
                 return 'Evening'
             } else {
                 return 'Night'
